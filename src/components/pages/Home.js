dba045b38c23ae2a603221f14149b796b0193534
Konny P Ho <conniebilac@gmail.com>
2023-03-22 21:56:51 -0400

    still working on UI design

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,33 @@
+// import React from 'react';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <p>
+//        My first react page.
+//       </p>
+//     </div>
+//   );
+// }
+
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-       My first react page.
-      </p>
-    </div>
+    <Container>
+      <Row className="justify-content-center align-items-center vh-100">
+        <Col md={6}>
+          <h1 className="text-center mb-4">Home Page</h1>
+          <p className="text-center">
+            My first React page with Bootstrap.
+          </p>
+          <div className="d-flex justify-content-center mt-4">
+            <Button variant="primary" href="#">Learn More</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
